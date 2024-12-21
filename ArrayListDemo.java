e0572314b176cc7a75fae00351e13369183c1677
Sakshi2316 <holesakshi23@gmail.com>
2024-12-21 20:19:07 +0530

    initil

---
+++ ArrayListDemo.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+class ArrayListDemo {
+
+    public static void main(String[] args) {
+        
+     ArrayList l = new ArrayList();
+     l.add("Abhi");
+     l.add("232019");
+     l.add("J");
+     l.add("null");
+
+     System.out.println(l);
+      l.remove(2);
+        System.out.println(l);
+
+      l.add(2, "Sakshi");
+      System.out.println(l);
+
+      l.add("JADHAV");
+      System.out.println(l);
+
+    }
+    
+}
